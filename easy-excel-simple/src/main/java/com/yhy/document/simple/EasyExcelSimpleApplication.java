ed5595a8baafe87c65a02ba1b59efa8481645f9f
颜洪毅 <yhyzgn@gmail.com>
2020-10-23 11:14:17 +0800

    gradle

---
+++ easy-excel-simple/src/main/java/com/yhy/document/simple/EasyExcelSimpleApplication.java
@@ -0,0 +1,12 @@
+package com.yhy.document.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExcelSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyExcelSimpleApplication.class, args);
+    }
+}
